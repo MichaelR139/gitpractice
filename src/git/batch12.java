9b76e627ada81660593ca7fdb1f7e73778f6eba1
MichaelR139 <michaelradvinsky@gmail.com>
2022-02-16 18:53:07 -0800

    the commit

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -3,8 +3,9 @@ package git;
 public class batch12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+System.out.println("we");
+System.out.println("jgjgjgj");
 	}
 
 }
